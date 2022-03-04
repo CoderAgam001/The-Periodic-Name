01d63f5cfe5423be407d5bdd2b8a54e60559aea7
Agamdeep Singh <agamdeep579@gmail.com>
2022-03-04 16:09:33 +0530

    Added the code

---
+++ elements.py
@@ -0,0 +1,168 @@
+# List of all Periodic elements from the Periodic Table in Alphabetical Order
+
+elements = {
+    # A
+    'Actinium': 'Ac',
+    'Aluminium': 'Al',
+    'Americium': 'Am',
+    'Antimony': 'Sb',
+    'Argon': 'Ar',
+    'Arsenic': 'As',
+    'Astatine': 'At',
+    
+    # B
+    'Barium': 'Ba',
+    'Berkelium': 'Bk',
+    'Berylium': 'Be',
+    'Bismuth': 'Bi',
+    'Bohrium': 'Bh',
+    'Boron': 'B',
+    'Bromine': 'Br',
+    
+    # C
+    'Cadmium': 'Cd',
+    'Calcium': 'Ca',
+    'Californium': 'Cf',
+    'Carbon': 'C',
+    'Cerium': 'Ce',
+    'Cesium': 'Cs',
+    'Chlorine': 'Cl',
+    'Chromium': 'Cr',
+    'Cobalt': 'Co',
+    'Copernicium': 'Cn',
+    'Copper': 'Cu',
+    'Curium': 'Cm',
+
+    # D
+    'Darmstadtium': 'Ds',
+    'Dubnium': 'Db',
+    'Dysprosium': 'Dy',
+
+    # E
+    'Einsteinium': 'Es',
+    'Erbium': 'Er',
+    'Europium': 'Eu',
+    
+    # F
+    'Fermium': 'Fm',
+    'Flerovium': 'Fl',
+    'Fluorine': 'F',
+    'Francium': 'Fr',
+    
+    # G
+    'Gadolinium': 'Gd',
+    'Gallium': 'Ga',
+    'Germanium': 'Ge',
+    'Gold': 'Au',
+    
+    # H
+    'Hafnium': 'Hf',
+    'Hassium': 'Hs',
+    'Helium': 'He',
+    'Holmium': 'Ho',
+    'Hydrogen': 'H',
+    
+    # I
+    'Indium': 'In',
+    'Iodine': 'I',
+    'Iridium': 'Ir',
+    'Iron': 'Fe',
+    
+    # K 
+    'Krypton': 'Kr',
+    
+    # L
+    'Lanthanum': 'La',
+    'Lawrencium': 'Lr',
+    'Lead': 'Pb',
+    'Lithium': 'Li',
+    'Livermorium': 'Lv',
+    'Lutetium': 'Lu',
+
+    # M
+    'Magnesium': 'Mg',
+    'Manganese': 'Mn',
+    'Meitnerium': 'Mt',
+    'Mendelevium': 'Md',
+    'Mercury': 'Hg',
+    'Molybdenum': 'Mo',
+    'Moscovium': 'Mc',
+
+    # N
+    'Neodymium': 'Nd',
+    'Neon': 'Ne',
+    'Neptunium': 'Np',
+    'Nickel': 'Ni',
+    'Nihonium': 'Nh',
+    'Niobium': 'Nb',
+    'Nitrogen': 'N',
+    'Nobelium': 'No',
+
+    # O
+    'Organesson': 'Og',
+    'Osmium': 'Os',
+    'Oxygen': 'O',
+
+    # P
+    'Palladium': 'Pd',
+    'Phosphorus': 'P',
+    'Platinum': 'Pt',
+    'Plutonium': 'Pu',
+    'Polonium': 'Po',
+    'Potassium': 'K',
+    'Praseodymium': 'Pr',
+    'Promethium': 'Pm',
+    'Protactinium': 'Pa',
+
+    # R
+    'Radium': 'Ra',
+    'Radon': 'Rn',
+    'Rhenium': 'Re',
+    'Rhodium': 'Rh',
+    'Roentgenium': 'Rg',
+    'Rubidium': 'Rb',
+    'Ruthenium': 'Ru',
+    'Rutherfordium': 'Rf',
+
+    # S
+    'Samarium': 'Sm',
+    'Scandium': 'Sc',
+    'Seaborgium': 'Sg',
+    'Selenium': 'Se',
+    'Silicon': 'Si',
+    'Silver': 'Ag',
+    'Sodium': 'Na',
+    'Strontium': 'Sr',
+    'Sulphur': 'S',
+
+    # T
+    'Tantalum': 'Ta',
+    'Technetium': 'Tc',
+    'Tellurium': 'Te',
+    'Tennessine': 'Ts',
+    'Terbium': 'Tb',
+    'Thallium': 'Tl',
+    'Thorium': 'Th',
+    'Thulium': 'Tm',
+    'Tin': 'Sn',
+    'Titanium': 'Ti',
+    'Tungsten': 'W',
+
+    # U
+    'Uranium': 'U',
+
+    # V
+    'Vanadium': 'V',
+
+    # X
+    'Xenon': 'Xe',
+
+    # Y
+    'Ytterbium': 'Yb',     
+    'Yttrium': 'Y', 
+
+    # Z
+    'Zirconium': 'Za',
+    'Zinc': 'Zn',
+    'Zirconium': 'Zr',
+}
